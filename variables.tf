a9ed5e57fe258c07dde860d2ba726f7e2d2db2ff
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-04 15:13:49 +0530

    roboshop msg

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "instances" {
+    type     = map
+    default = {
+        mongodb  = "t3.small"
+        redis    = "t3.micro"
+        mysql    = "t3.small"
+        rabbitmq = "t3.micro"
+        catalogue= "t3.micro"
+        user     = "t3.micro"
+        cart     = "t3.micro"
+        shipping = "t3.small"
+        payment  = "t3.micro"
+        dispatch = "t3.micro"
+        frontend = "t3.micro"
+    }
+}
+variable "allow_all" {
+   type = string
+   default = "sg-06b1b57b365846051"
+}
+variable "zone_id" {
+    default = "Z0691278WUXMPUSZGPL9"
+}
+variable "domain_name" {
+  default = "lingaiah.online"
+}
